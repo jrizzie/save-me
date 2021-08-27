0466e0c9e3b9566bc7453b9dbc2f8756a478bdc3
jrizzie <58239313+jrizzie@users.noreply.github.com>
2021-08-27 07:15:07 +0000

    Add files via upload

---
+++ 1.0_EDITH.sh
@@ -0,0 +1,88 @@
+#! bin/bash
+# To run type "bash 1.0_EDITH.sh"
+
+echo "Hello! My name is EDITH.  "
+#  EDITH = Even Dead... I Am The Hero.  :)
+sleep 1.5
+echo ' '
+echo '  ^    ^' 
+echo "    ..  "
+echo "  \____/ "
+echo '   '
+sleep 2
+echo "This script has 2 parts..."
+sleep 1  
+echo "Part 1 will stop and restart all  Security Onion (SO) services"
+sleep 1
+echo "Part 2 will start up additional services (Moloch, MISP, and the Hive)."
+sleep 1
+read -p "Press [c] to continue, or press anything else to quit: " continue 
+if [[ $continue = c ]] ; then
+
+	sleep .5
+	read -p "Does SO need to be updated? Please enter [y/n]" answer
+		if [[ $answer = y ]] ; then
+
+			echo sudo so-stop
+			sudo so-stop
+			sleep 3
+			echo sudo soup
+			sudo soup
+			sleep 3
+			echo sudo so-start
+			sudo sudo so-start
+
+		fi
+#############################################################################
+# 				just for fun                                #
+#############################################################################
+#
+clear
+echo 'now its time for some fun...'
+sleep 2
+clear
+bash /home/jrizzie/fun/chloe.sh
+#
+#############################################################################
+#                               just for fun                                #
+#############################################################################
+clear
+	read -p "Start Moloch/Hive/Misp? Enter [y/n]: " last
+	if [[ $last = y ]] ; then
+		echo Starting Moloch...
+		sleep 1 
+		echo sudo systemctl start molochcapture
+		sudo systemctl start molochcapture
+		sleep 1
+		echo .
+		sleep .5	
+		echo ..
+		sleep .5
+		echo ...
+		sleep 3
+		echo sudo systemctl start molochviewer
+		sudo systemctl start molochviewer
+		sleep 1
+		echo .
+		sleep .5
+		echo ..
+		sleep .5
+		echo ...
+		sleep 3
+		clear
+		echo 'starting The Hive - (in Docker)...'
+		cd /docker/thehive && docker-compose up -d
+		cd ~
+		echo .
+		sleep 1
+		echo ..
+		sleep 1
+		echo ...
+		echo 'Moloch should be available at 127.0.0.1:8005'
+		echo 'user:  jrizzie'
+		echo 'pass:  golicksomeB@115'
+		echo 'The Hive should be available at 127.0.0.1:9000'
+		echo 'user:  admin'
+		echo 'pass:  golicksomeB@115'
+		fi
+fi
---
+++ chloe.sh
@@ -0,0 +1,164 @@
+#############################################################################
+#                               just for fun                                #
+#############################################################################
+sleep .25
+echo ''
+sleep .25
+echo '  ****   *   *   *     ****   *****'
+sleep .25
+echo ' **  **  *   *   *    **  **  *'
+sleep .25
+echo ' *       *****   *    *    *  ****'
+sleep .25
+echo ' *       *****   *    *    *  *'
+sleep .25
+echo ' **  **  *   *   *    **  **  *'
+sleep .25
+echo '  ****   *   *   ***   ****   *****'
+sleep .25
+echo ''
+sleep .25
+echo ''
+sleep .25
+echo '     *****  ****         *'
+sleep .25
+echo '       *    *           * *'
+sleep .25
+echo '       *    ****       *****'
+sleep .25
+echo '       *       *      *     *'
+sleep .25
+echo '     *****  ****     *       *'
+sleep .25
+echo ''
+sleep .25
+echo ''
+sleep .25
+echo '      ***    ***   ***   *   *'
+sleep .25
+echo '      *  *    *   **  *  *   *'
+sleep .25
+echo '      ***     *   **     *****'
+sleep .25
+echo '      *  *    *     **   *****'
+sleep .25
+echo '      *  *    *   *  **  *   *'
+sleep .25
+echo '      ****   ***   ***   *   *'
+sleep .25
+echo ''
+sleep .25
+sleep 3
+clear
+sleep 2
+########## making it flash ##########
+echo ''
+echo '  ****   *   *   *     ****   *****'
+echo ' **  **  *   *   *    **  **  *'
+echo ' *       *****   *    *    *  ****'
+echo ' *       *****   *    *    *  *'
+echo ' **  **  *   *   *    **  **  *'
+echo '  ****   *   *   ***   ****   *****'
+echo ''
+echo ''
+echo '     *****  ****         *'
+echo '       *    *           * *'
+echo '       *    ****       *****'
+echo '       *       *      *     *'
+echo '     *****  ****     *       *'
+echo ''
+echo ''
+echo '      ***    ***   ***   *   *'
+echo '      *  *    *   **  *  *   *'
+echo '      ***     *   **     *****'
+echo '      *  *    *     **   *****'
+echo '      *  *    *   *  **  *   *'
+echo '      ****   ***   ***   *   *'
+echo ''
+sleep .75
+clear
+sleep .75
+echo ''
+echo '  ****   *   *   *     ****   *****'
+echo ' **  **  *   *   *    **  **  *'
+echo ' *       *****   *    *    *  ****'
+echo ' *       *****   *    *    *  *'
+echo ' **  **  *   *   *    **  **  *'
+echo '  ****   *   *   ***   ****   *****'
+echo ''
+echo ''
+echo '     *****  ****         *'
+echo '       *    *           * *'
+echo '       *    ****       *****'
+echo '       *       *      *     *'
+echo '     *****  ****     *       *'
+echo ''
+echo ''
+echo '      ***    ***   ***   *   *'
+echo '      *  *    *   **  *  *   *'
+echo '      ***     *   **     *****'
+echo '      *  *    *     **   *****'
+echo '      *  *    *   *  **  *   *'
+echo '      ****   ***   ***   *   *'
+echo ''
+sleep .75
+clear
+sleep .75
+echo ''
+echo '  ****   *   *   *     ****   *****'
+echo ' **  **  *   *   *    **  **  *'
+echo ' *       *****   *    *    *  ****'
+echo ' *       *****   *    *    *  *'
+echo ' **  **  *   *   *    **  **  *'
+echo '  ****   *   *   ***   ****   *****'
+echo ''
+echo ''
+echo '     *****  ****         *'
+echo '       *    *           * *'
+echo '       *    ****       *****'
+echo '       *       *      *     *'
+echo '     *****  ****     *       *'
+echo ''
+echo ''
+echo '      ***    ***   ***   *   *'
+echo '      *  *    *   **  *  *   *'
+echo '      ***     *   **     *****'
+echo '      *  *    *     **   *****'
+echo '      *  *    *   *  **  *   *'
+echo '      ****   ***   ***   *   *'
+echo ''
+sleep 3
+##########   end of flash   ##########
+clear
+sleep 1
+echo ' '
+echo ' '
+echo ' '
+echo '		 **		**	'
+echo '		 **             **      '
+echo ' '
+echo ' '
+echo '		 \		 /	'
+echo '		  \             /       '
+echo '		   \___________/        '
+sleep 4
+clear
+echo ' jk...  you know i love you babe.  <3  '
+sleep 1
+echo ' '
+echo ' '
+echo '        ____    ____'
+echo '       /    \  /    \'
+echo '       |     \/     |'
+echo '        \          /'
+echo '         \        /'
+echo '          \      /'
+echo '           \    /'
+echo '            \  /'
+echo '             \/'
+echo ' ' 
+sleep 4
+#############################################################################
+#                               just for fun                                #
+#############################################################################
+
